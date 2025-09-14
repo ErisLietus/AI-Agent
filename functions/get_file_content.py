129874d2d67eebb451dccc300656177bd6d3f04f
Eris Lietus <yukitoichijo@gmail.com>
2025-09-14 16:10:30 +0100

    Second Test

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,13 +1,23 @@
-def get_file_content(working_directory, directory="."):
+from config import MAX_CHARS
+import os
+
+def get_file_content(working_directory, file_path):
     try:
-        target_path = os.path.join(working_directory, directory)
         abs_working = os.path.abspath(working_directory)
-        abs_target = os.path.abspath(target_path)
+        abs_target = os.path.abspath(os.path.join(working_directory, file_path))
 
         if not (abs_target == abs_working or abs_target.startswith(abs_working + os.sep)):
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
 
-        if not os.path.isdir(abs_target):
-            return f'Error: "{directory}" is not a directory'
-        except Exception as e:
-            return f"Error: {e}"
+        if not os.path.isfile(abs_target):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        with open(abs_target, "r") as f:
+            content = f.read(MAX_CHARS + 1)
+
+        if len(content) == MAX_CHARS + 1:
+            return content[:MAX_CHARS] + f'[...File "{file_path}" truncated at 10000 characters]'
+        return content
+
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,13 +1,14 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-def show(title, wd, dir_):
-    result = get_files_info(wd, dir_)
+def show(title, wd, file_path):
+    result = get_file_content(wd, file_path)
     print(f"Result for {title}:")
     for line in result.splitlines():
         print(f"  {line}")
 
 if __name__ == "__main__":
-    show("current directory", "calculator", ".")
-    show("'pkg' directory", "calculator", "pkg")
-    show("'/bin' directory", "calculator", "/bin")
-    show("'../' directory", "calculator", "../")
+    show("main", "calculator", "main.py")
+    show("outside","calculator", "pkg/calculator.py")
+    show("missing","calculator", "/bin/cat")
+    show("error","calculator", "pkg/does_not_exist.py")
+   
